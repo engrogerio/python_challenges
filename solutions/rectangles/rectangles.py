0c4d028031d2ba2211c57a51a868edc747f814b0
rsilva <rsilva@guardian.com>
2020-05-18 18:30:54 -0300

    add timeslots

--- solutions/rectangles/rectangles.py
+++ solutions/rectangles/rectangles.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame import *
+import sys
 
 class Point:
     def __init__(self, x, y):
@@ -19,22 +22,22 @@ class Point:
         return not(self == other)
 
 class Rectangle:
-    def __init__(self, point1, point2, point3, point4):
+    def __init__(self, point1, point2): #, point3, point4):
         self.point1 = point1
         self.point2 = point2
-        self.point3 = point3
-        self.point4 = point4
+        # self.point3 = point3
+        # self.point4 = point4
 
     def __repr__(self):
-        return "[(%s, %s), (%s, %s), (%s, %s), (%s, %s)]" % (self.point1.x, self.point1.y, self.point2.x, self.point2.y, self.point3.x, self.point3.y, self.point4.x, self.point4.y)
+        return "[(%s, %s), (%s, %s)]" #, (%s, %s), (%s, %s)]" % (self.point1.x, self.point1.y, self.point2.x, self.point2.y) #, self.point3.x, self.point3.y, self.point4.x, self.point4.y)
 
     def __hash__(self):
-        return hash((self.point1, self.point2, self.point3, self.point4))
+        return hash((self.point1, self.point2)) #, self.point3, self.point4))
 
     def __eq__(self,other):
         # A rectangle is equal to other rectangle when all points of the first exists on the other.
-        other_points = [other.point1, other.point2, other.point3, other.point4]
-        return self.point1 in other_points and self.point2 in other_points and self.point3 in other_points and self.point4 in other_points
+        other_points = [other.point1, other.point2] #, other.point3, other.point4]
+        return self.point1 in other_points and self.point2 in other_points #and self.point3 in other_points and self.point4 in other_points
 
     def __ne__(self, other):
         return not (self == other)
@@ -88,36 +91,89 @@ def get_rectangles(points):
                 if len(intersect) == 2:
                     if intersect not in intersects:
                         intersects.append(intersect)
-                        rectangle = Rectangle(p1, p2, list(intersect)[0], list(intersect)[1])
+                        rectangle = Rectangle(p1, p2) #, list(intersect)[0], list(intersect)[1])
                         if rectangle not in rectangles:
                             rectangles.append(rectangle)
     return rectangles
 
 
-
-if __name__ == "__main__":
+def main():
     points = [
-            Point(10, 10),
-            Point(10, 20),
-            Point(10, 30),
-            Point(20, 10),
-            Point(20, 20),
-            Point(20, 30),
-            Point(30, 10),            
-            Point(30, 20),
-            Point(30, 30),
-            Point(40, 10),
-            Point(40, 20),
-            Point(40, 30),
-            Point(50, 10),  
-            Point(50, 20),
-            Point(50, 30),
-            Point(60, 10),
-            Point(60, 20),
-            Point(60, 30)
+            Point(100, 100),
+            Point(100, 200),
+            Point(100, 300),
+            Point(200, 100),
+            Point(200, 200),
+            Point(200, 300),
+            Point(300, 100),            
+            Point(300, 200),
+            Point(300, 300),
+            Point(400, 100),
+            Point(400, 200),
+            Point(400, 300),
+            Point(500, 100),  
+            Point(500, 200),
+            Point(500, 300),
+            Point(600, 100),
+            Point(600, 200),
+            Point(600, 300)
             ]
 
-    answer = get_rectangles(points)
+    rectangles = get_rectangles(points)
 
-    print(len(answer))
+    pygame.init()
+    pygame.font.init() 
+ 
+    DISPLAY=pygame.display.set_mode((800,600),0,32)
+
+    WHITE = (255,255,255)
+    BLUE = (0,0,255)
+    RED = (255, 0, 0)
+    DISPLAY.fill(WHITE)
+    
+    myfont = pygame.font.Font(pygame.font.get_default_font(), 36)
+
+    for n, rect in enumerate(rectangles):
+        color = (n, n*2, n*2)
+        pygame.draw.rect(DISPLAY, color, (rect.point1.x, rect.point1.y, rect.point2.x, rect.point2.y),2)
+        myfont.render(str(n), True, color)
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+def main2():
+    pygame.init()
+    
+    DISPLAY = pygame.display.set_mode((800, 800),0 ,32)
+
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    
+    myfont = pygame.font.Font(pygame.font.get_default_font(), 36)
+    pygame.font.init()
+   
+    DISPLAY.fill(GREEN)
+    pygame.draw.rect(DISPLAY, RED, (10, 10, 200, 200))
+    pygame.draw.rect(DISPLAY, BLUE, (210, 210, 400, 400), 2)
+    textsurface = myfont.render('ROGERIO', False, (0, 0, 0))
+
+    DISPLAY.blit(textsurface, (10, 10))
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+
+
+main2()
 
